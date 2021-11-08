73af0df17a2258523e51119f608367a470ac76c5
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-11-08 20:32:38 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+//   ALGORITMOS SECUENCIALES
+
+//Ejercicio: Suma de Dos Números
+
+//Realizamos la operación y mostramos el resultado
+
+//let n1 : number =5;
+//let n2: number =5;
+//let sum : number;
+
+//sum = n1 + n2;
+
+//console.log ("el resultado es: "+sum);
+
+//------------------------------------------------
+
+//Ejercicio: Área del Rectángulo
+//Volvamos a implementar el proceso que calcula el área de un rectángulo pero para cualquier base o altura
+//El usuario debe ingresar la base y altura por teclado
+//El área debe guardarse en una variable
+//El resultado debe ser mostrado por pantalla
+
+//let bas: number = 10;
+//let alt: number = 6;
+//let a: number;
+//let multi: number;
+
+//multi = bas * alt;
+
+//a = multi;
+
+//console.log("el are del rectangulo es: " + a);
+//***** */
+//-----------------------------------------------------------------
+
+//Problema: Autos de Carrera
+
+//En una prueba, un piloto tiene que completar 4 vueltas
+//Se necesita un programa que permita ingresar
+// por teclado el tiempo de cada vuelta
+//El programa debe retornar el tiempo total
+//y el promedio de vuelta
+
+//let v1: number = 10;
+//let v2: number = 5;
+//let v3: number = 10;
+//let v4: number = 2;
+
+//let sTotal: number;
+//let prom: number;
+
+//sTotal = v1 + v2 + v3 + v4;
+//prom = sTotal / 4;
+
+//console.log("la suma total es: " + sTotal);
+//console.log("El promedio es: " + prom);
+
+//-----------------------------------------------------
